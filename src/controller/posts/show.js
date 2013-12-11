e3c4964a2f446ccdc5eaf22a14497f09c9e92121
Dave Turissini <davidairborne@gmail.com>
2013-12-10 20:11:48 -0800

    Adding support for new REST data

--- src/controller/posts/show.js
+++ src/controller/posts/show.js
@@ -1,6 +1,7 @@
 define(function (require) {
 
 	var resourceFetch = require('resource/fetch');
+	var Photo = require('model/Photo');
 
 	return function (route) {
 		var resourcePath = 'posts/' + route.params.postSlug;
@@ -10,20 +11,15 @@ define(function (require) {
 
 		promise = promise.then(function (e) {
 			var data = {};
-			var flickrImage;
+			var photo;
 			data.post = JSON.parse(e);
-			flickrImage = data.post.photo;
+			photo = new Photo(data.post.photo);
 
-			data.post.hero_image = 'http://farm' + flickrImage.flickr_farm + '.staticflickr.com/' + flickrImage.flickr_server + '/' + flickrImage.flickr_id + '_' + flickrImage.flickr_secret + '_b.jpg';
+			data.post.hero_image = photo.getUrl('b');
 			data.title = data.post.title + ' - turissini.io';
 			data.ogTitle = data.title.replace(' - turissini.io', '');
-			data.ogDescription = data.description = 'New York City is a great place to visit. Just beware of closing subway doors.';
+			data.ogDescription = data.description = data.post.description;
 			data.ogImage = data.post.hero_image;
-			data.post.facts = [{
-				text:'The subway first started regular service in 1910.'
-			},{
-				text:'1.7 billion people rode the New York subway in 2012, the highest total since 1950.'
-			}];
 
 
 			return data;
