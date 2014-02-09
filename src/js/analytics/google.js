ea508c6b1cdce24439d483428ba4d18301289f25
Dave Turissini <davidairborne@gmail.com>
2014-02-09 00:18:04 -0800

    Moving around google analytics

--- src/js/analytics/google.js
+++ src/js/analytics/google.js
@@ -15,15 +15,5 @@ if (process.browser === true) {
 		
 	};
 
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-31293401-2']);
-	trackPageView();
-
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-
 	exports.track = trackPageView;
 }
